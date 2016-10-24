013cc80e8ad64023adff387f50b9fe5f9959b7d8
Kfir Silis <silis25@gmail.com>
2016-10-24 22:04:52 +0300

    Made a map of Israel

--- myproject.py
+++ myproject.py
@@ -5,7 +5,7 @@ from flask import render_template
 app = Flask(__name__)
 
 def get_csv():
-    csv_path = './static/la-riots-deaths.csv'
+    csv_path = './static/fiber-cuts.csv'
     csv_file = open(csv_path, 'r')
     csv_obj = csv.DictReader(csv_file)
     csv_list = list(csv_obj)
